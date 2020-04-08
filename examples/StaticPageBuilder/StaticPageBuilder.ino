1ef66492598de5e46b6300bd3b5968d0efb1d645
DaleGia <31597257+DaleGia@users.noreply.github.com>
2020-04-08 23:10:44 +0200

    Update StaticPageBuilder.ino
    Updating example to conform with new SPIFFS implementation.

--- examples/StaticPageBuilder/StaticPageBuilder.ino
+++ examples/StaticPageBuilder/StaticPageBuilder.ino
@@ -3,10 +3,12 @@ ESPStringTemplate example
 
 Copyright (c) 2020 Dale Giancono. All rights reserved..
 This file is a sample application of the ESPStringTemplate class
-that uses HTML string elements stored in flash to create static 
-web pages. The title of the web page can be edited by using the 
-lineEdit element and relevent submit buttons. It uses the 
-ESPAsyncWebServer to serve these statically created web pages.
+that uses HTML string elements stored flash to build web page stored
+and accessed using SPIFFS. The example serves a webpage with a title, 
+a line edit, and a submit button. The title of the page can be 
+changed by submiting a string with the line edit. Each time the 
+submit button is pressed the webpage is statically created using 
+the submited line edit string as the title.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -27,6 +29,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <ESPAsyncWebServer.h>
 #include "ESPStringTemplate.h"
 
+void buildPage(void);
+
 /* Store different HTML elements in flash. Descriptions of the various 
 tokens are included above each element that has tokens.*/
 static const char _PAGEHEADER[] PROGMEM = "<!DOCTYPE html><html><body>";
@@ -36,93 +40,77 @@ static const char _TITLE[]      PROGMEM = "<hr><h1>%TITLE%</h1><hr>";
 /*%VALUE% token refers to the text that will be inside the button
  as well as the parameter value used in the HTTP_GET request. */
 static const char _BUTTON[]     PROGMEM = "<form action=\"%ACTION%\"><input type=\"submit\" value=\"%VALUE%\" name=\"event\"></form>";
-/*%ACTION% token refers to the uri that is called from the form.  */
 /*%LINEEDITNAME% token refers to the text that will be inside the lineedit
  as well as the parameter value used in the HTTP_GET request. */
-/*%SUBMITVALUE% token refers to the text that will be inside the submit button
-as well as the parameter value used in the HTTP_GET request. */
-static const char _LINEEDIT[]   PROGMEM = "<form action=\"%ACTION%\"><input type=\"text\" name=\"%LINEEDITNAME%\"><input type=\"Submit\"  value=\"%SUBMITVALUE%\" name=\"event\"></form>";
+static const char _LINEEDIT[]   PROGMEM = "<form action=\"/\"><input type=\"text\" name=\"%LINEEDITNAME%\"><input type=\"Submit\"  value=\"submit\" name=\"event\"></form>";
 static const char _LINE[]       PROGMEM = "<hr>";
 static const char _PAGEFOOTER[] PROGMEM = "</body></html>";
 
-/* Declare buffer that will be used to fill with the string template. Care \
-must be taken to ensure the buffer is large enough.*/
-static char pageBuffer[400];
-
 /* Create webserver instance for serving the StringTemplate example. */
 AsyncWebServer server(80);
-/* Define the IP address, gateway and subnet for the access point. */
-IPAddress staticIP(192,168,4,1);
-IPAddress gateway(192,168,4,1);
-IPAddress subnet(255,255,255,0);
+/* Create the filename which the page template will be saved under in spiffs */
+static const char htmlTemplateFileName[] = "/stringTemplate/template.html";
 
 void setup() 
 {
-  Serial.begin(115200);
+  /* Start SPIFFS so it can be used by ESPStringTemplate */
+  SPIFFS.begin();
   /* Configure access point with static IP address */
-  WiFi.softAPConfig(staticIP, gateway, subnet);
+  WiFi.softAPConfig(
+    IPAddress(192,168,4,1),
+    IPAddress(192,168,4,1),
+    IPAddress(255,255,255,0));
   WiFi.softAP("StringTemplate HTMLBuilder Test");
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin();
-  Serial.println(1); 
   /* Define the handler for when the server receives a GET request for the root uri. */
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
   {
     /* Create instance of StringTemplate, TokenStringPair, and a TokenStringPair array
     that will be used to build the site.*/
-    ESPStringTemplate testTemplate(pageBuffer, sizeof(pageBuffer));
+    ESPStringTemplate testTemplate(htmlTemplateFileName);
     TokenStringPair pair;
-    TokenStringPair pairArray[3];
+    TokenStringPair pairArray[2];
 
     /* Add page header string to the template. */
-    testTemplate.add(FPSTR(_PAGEHEADER));
+    testTemplate.add_P(_PAGEHEADER);
     /* Set tokenStringPair to replace %TITLE% token with "StringTemplate Example"*/
     /* This is set here as it is meant to be the default value */
-
-    pair.setPair("%TITLE%", "ESPStringTemplate Example");
-
+    String title = "StringTemplate Example";
     /* Check with a parameter named "event" was included in the GET request. */ 
     if(request->hasParam("event"))
     {
       AsyncWebParameter* p = request->getParam("event");
       /* Check if the value of the "event" parameter is "lineeditsubmit". If it is, 
       get the value of the parameter amd set tokenStringPair to replace %TITLE% token with the value */
-      if(p->value() == "Submit")
+      if(p->value() == "submit")
       {
         if(request->hasParam("lineedittext"))
         {
           p = request->getParam("lineedittext");
+          title = p->value();
         }
-        pair.setPair("%TITLE%", p->value().c_str());
       }
     }
     /* Add the __TITLE string to the StringTemplate and use the TokenStringPair to replace
      *  specific token strings.*/
-    testTemplate.add(FPSTR(_TITLE), &pair);
-
-  
-    /* Set the various TokenStringPairs in the pairArray to replace tokens in the _LINEEDIT string */
-    pairArray[0] = TokenStringPair("%ACTION%", "/");
-    pairArray[1] = TokenStringPair("%LINEEDITNAME%", "lineedittext");
-    pairArray[2] = TokenStringPair("%SUBMITVALUE%", "Submit");
+    testTemplate.add_P(_TITLE, "%TITLE%", title.c_str());
     /* Add the _LINEEDIT string to the StringTemplate and use the TokenStringPair to replace
      *  specific token strings.*/
-    testTemplate.add(FPSTR(_LINEEDIT), pairArray, 3);
+    testTemplate.add_P(_LINEEDIT, "%LINEEDITNAME%", "lineedittext");
     /* Add the _LINE string to the StringTemplate */
-    testTemplate.add(FPSTR(_LINE));
-
+    testTemplate.add_P(_LINE);
       
     /* Set the various TokenStringPairs in the pairArray to replace tokens in the _LINEEDIT string */
     pairArray[0] = TokenStringPair("%ACTION%", "/");
     pairArray[1] = TokenStringPair("%VALUE%", "reset");
     /* Add the _BUTTON string to the StringTemplate and use the TokenStringPair to replace
      *  specific token strings.*/
-    testTemplate.add(FPSTR(_BUTTON), pairArray, 2);
+    testTemplate.add_P(_BUTTON, pairArray, 2);
     /* Add the _PAGEFOOTER string to the StringTemplate */
-    testTemplate.add(FPSTR(_PAGEFOOTER));   
-    
-    const char* page =  testTemplate.get();
-    request->send(200, "text/html", page);
+    testTemplate.add_P(_PAGEFOOTER);   
+    /* Send the webpage from SPIFFS where it is stored. */
+    request->send(SPIFFS, testTemplate.getFilename(), "text/html");
   });
 
   /* Begin the web server */
