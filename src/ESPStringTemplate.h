0185868471c4b84bd94ecdf24f6f09a7ac38ceee
DaleGia <31597257+DaleGia@users.noreply.github.com>
2020-04-08 23:07:11 +0200

    Updating interface for usage with SPIFFS
    Changed to SPIFFS usage rather than static char* usage. This has a performance penalty, and also makes the library a little more unsafe, but it now uses very little RAM and FLASH memory even for very large web pages.

--- src/ESPStringTemplate.h
+++ src/ESPStringTemplate.h
@@ -3,9 +3,10 @@ ESPStringTemplate.h
 
 Copyright (c) 2020 Dale Giancono. All rights reserved..
 This file defines the ESPStringTemplate class. It can be used
-to build the conent of statically allocated strings and have "tokens" 
-within those strings replaced with substrings. It is particularly 
-useful as a static web page generator.
+to build the content of strings and have "tokens" 
+within those strings replaced with substrings. The content is stored using 
+SPIFFS. It is particularly useful as a static web page generator that 
+uses low amounts of RAM and FLASH due to it's SPIFFs ussage.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -23,13 +24,12 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #ifndef ESPSTRINGTEMPLATE_H_
 #define ESPSTRINGTEMPLATE_H_
 
-#include <WString.h>
-
+#include <pgmspace.h>
+#include <FS.h>
 /* 
   The TokenStringPair class can be used with the StringTemplate class
   to provide a simple way to replace "tokens" within a string with other
-  strings. Pointers to TokenStringPair instances are used as an argument
-  in many StringTemplate methods.
+  strings.
 */
 class TokenStringPair
 {
@@ -48,36 +48,29 @@ class TokenStringPair
 };
 
 /*
- * The StringTemplate class can be used to add strings to a statically
- * allocated buffer. "Tokens" within added strings can be replaced with 
- * other strings.
+ * The StringTemplate class can be used to add strings to a SPIFFS file that is
+ * crated in the ESPStringTemplate constructor. "Tokens" within strings to be 
+ * added to the before mentioned file can be replaced with other strings.
  */
 class ESPStringTemplate
 {
   public:
-    ESPStringTemplate(char* templateBuffer, uint32_t templateBufferSize);
+    ESPStringTemplate(const char* filename);
   
-    bool add_P(PGM_P  stringToAdd);
-    bool add(const char* stringToAdd);
-    bool add(const char* stringToAdd, const char* token, const char* string);
-    bool add_P(PGM_P stringToAdd, const char* token, const char* string);
+    void add_P(PGM_P  stringToAdd);
+    void add(const char* stringToAdd);
+    void add(const char* stringToAdd, const char* token, const char* string);
+    void add_P(PGM_P stringToAdd, const char* token, const char* string);
 
-    bool add(const char* stringToAdd, TokenStringPair pairList[], size_t numberOfPairs);
-    bool add_P(PGM_P stringToAdd, TokenStringPair pairList[], size_t numberOfPairs);
+    void add(const char* stringToAdd, TokenStringPair pairList[], size_t numberOfPairs);
+    void add_P(PGM_P stringToAdd, TokenStringPair pairList[], size_t numberOfPairs);
 
     void clear(void);
-    const char* get(void);
-    bool isOverflow(void);
+    const char* getFilename(void);
     
   private:
-    char* buffer;
-    char* bufferEndPointer;
-    uint32_t bufferLength;
-    bool overflowFlag;
-  
-    size_t bufferLeft(void);
-    void handleOverflow(void);
-    bool replace(char* stringToEdit, const char* oldSubstring, const char* newSubstring);
+    File content;
+    const char* filename;
 };
 
 #endif /*ESPSTRINGTEMPLATE_H_*/
